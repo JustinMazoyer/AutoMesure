00eaa0606a82553ef598186766b8a0964deb2b11
mmarty01 <mmarty01@cst-tp-b10313>
2020-02-28 17:32:48 +0100

    Changement de package

---
+++ src/main/java/automesure/dao/DispositifFacade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package automesure.dao;
+
+import com.mycompany.automesure1.Dispositif;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Justin
+ */
+public class DispositifFacade extends AbstractFacade<Dispositif> {
+    
+	@PersistenceContext(unitName = "comptoirs")
+	private EntityManager em;
+
+	protected EntityManager getEntityManager() {
+		return em;
+	}
+
+	public DispositifFacade() {
+		super(Dispositif.class);
+	}
+	
+}
+
